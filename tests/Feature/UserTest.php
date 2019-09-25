ee733c4aeebc11fa423fbd23c5d9254890b1ca88
krukovskiy <vladimir.kruk@computools.com>
2019-09-25 14:18:51 +0300

    Initial commit

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = factory(User::class)->create();
+
+    }
+}
